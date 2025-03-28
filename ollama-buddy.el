5f1511e9b74c6921f6aa7e0ac26aed1ca367a787
james dyer <captainflasmr@gmail.com>
2025-03-28 09:09:04 +0000

    Updates README and CHANGELOG

--- ollama-buddy.el
+++ ollama-buddy.el
@@ -1,7 +1,7 @@
 ;;; ollama-buddy.el --- Ollama Buddy: Your Friendly AI Assistant -*- lexical-binding: t; -*-
 ;;
 ;; Author: James Dyer <captainflasmr@gmail.com>
-;; Version: 0.9.13
+;; Version: 0.9.15
 ;; Package-Requires: ((emacs "28.1"))
 ;; Keywords: applications, tools, convenience
 ;; URL: https://github.com/captainflasmr/ollama-buddy
