8ca99011498ff6e07923d13615bf590dadfa4c9e
james dyer <captainflasmr@gmail.com>
2025-03-30 16:04:54 +0100

    Always open chat in the points windows

--- ollama-buddy.el
+++ ollama-buddy.el
@@ -2891,5 +2891,8 @@ When the operation completes, CALLBACK is called with no arguments if provided."
 
 (setq org-return-follows-link t)
 
+(add-to-list 'display-buffer-alist
+             '("\\*Ollama Buddy Chat" display-buffer-same-window))
+
 (provide 'ollama-buddy)
 ;;; ollama-buddy.el ends here
