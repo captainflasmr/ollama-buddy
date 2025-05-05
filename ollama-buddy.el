f7959e58c7a8545620c8a847867b61aa38fa53b6
james dyer <captainflasmr@gmail.com>
2025-05-05 13:06:47 +0100

    Updated READMEs

--- ollama-buddy.el
+++ ollama-buddy.el
@@ -1,7 +1,7 @@
 ;;; ollama-buddy.el --- Ollama LLM AI Assistant with ChatGPT, Claude, Gemini and Grok Support -*- lexical-binding: t; -*-
 ;;
 ;; Author: James Dyer <captainflasmr@gmail.com>
-;; Version: 0.9.42
+;; Version: 0.9.43
 ;; Package-Requires: ((emacs "28.1"))
 ;; Keywords: applications, tools, convenience
 ;; URL: https://github.com/captainflasmr/ollama-buddy
